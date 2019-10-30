d7e264e6b438d82090e993c1c6eaf634cf626af4
Philipp Hennersdorf <Philipp.Hennersdorf@encoway.de>
2019-10-30 15:30:58 +0100

    forms

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -1,29 +1,112 @@
 import React, {Component} from 'react';
+import axios from '../../../axios-orders';
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.css'
 
 class ContactData extends Component {
  state= {
-     name: '',
-     email: '',
-     address: {
-         street: '',
-         postalCode: ''
-     }
+    orderForm: {
+        name: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Your Name'
+            },
+            value: ''
+        },
+        street: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Street'
+            },
+            value: ''
+        },
+        zipCode:{
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'ZIP Code'
+            },
+            value: ''
+        },
+        country: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Country'
+            },
+            value: ''
+        },
+        email: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'email',
+                placeholder: 'Your E-Mail'
+            },
+            value: ''
+        },
+        deliveryMethod: {
+            elementType: 'select',
+            elementConfig: {
+                options: [
+                    {value: 'fastest', displayValue: 'Fastest'},
+                    {value: 'cheapest', displayValue: 'Cheapest'}
+                ]
+            },
+            value: ''
+        },
+    },
+    loading: false
  }
 
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({loading:true});
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+        }
+        axios.post('/orders.json', order)
+        .then(response => {
+            this.setState({loading: false,});
+            this.props.history.push('/');
+        })
+        .catch(error => {
+            this.setState({loading: false})
+        });
+        console.log(this.props.ingredients);
+    }
+
     render () {
+        const fromElementsArray =[];
+        for (let key in this.state.orderForm) {
+            fromElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+        let form = ( 
+        <form>
+            {fromElementsArray.map(formElement => (
+                <Input 
+                    key={formElement.id}
+                    elementType={formElement.config.elementType} 
+                    elementConfig={formElement.config.elementConfig} 
+                    value={formElement.config.value} />
+            ))}
+            <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        </form>);
+        if (this.state.loading) {
+            form = <Spinner />
+        }
         return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
-                <form>
-                    <input className={classes.Input} type="text" name='name' placeholder='Your name' />
-                    <input className={classes.Input} type='text' name='email' placeholder='Your email' />
-                    <input className={classes.Input} type='text' name='street' placeholder='Street' />
-                    <input className={classes.Input} type='text' name= 'postalCode' placeholder='Postal Code' />
-                    <Button btnType="Success">ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
