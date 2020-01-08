9413c12cc1d857e9ab767b84b74cb20b1cbbde83
Philipp Hennersdorf <Philipp.Hennersdorf@encoway.de>
2020-01-08 15:11:03 +0100

    added authentification

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -5,6 +5,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import {Route, Switch } from 'react-router-dom';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
 
@@ -22,6 +23,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path='/orders' component= {Orders}/>
+            <Route path='/auth' component={Auth}/>
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
